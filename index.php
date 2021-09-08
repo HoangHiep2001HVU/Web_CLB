949dd85b8c911ed357c2f645827303fca83c3c44
HoangHiep2001HVU <84690670+HoangHiep2001HVU@users.noreply.github.com>
2021-09-08 09:21:56 +0700

    tạo project

---
+++ index.php
@@ -0,0 +1,20 @@
+<!Doctype html>
+<header>
+	<title>CLB Tin Học</title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="public/css/css.css" />
+</header>
+<body>
+	<div id="menu">
+		
+	</div>
+	
+	<div id="body">
+	
+	</div>
+	
+	<div id="footer">
+	
+	</div>
+</body>
+</html>
