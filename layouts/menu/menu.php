20c0eacdec5cf67c6f3423db4a4d8b863afb6000
HoangHiep2001HVU <84690670+HoangHiep2001HVU@users.noreply.github.com>
2021-09-13 13:21:05 +0700

    them menu

--- layouts/menu/menu.php
+++ layouts/menu/menu.php
@@ -5,7 +5,16 @@
 			<li><a href="index.php">Trang chủ</a></li>
             <li><a href="#">Giới thiệu</a></li>
             <li><a href="#">Tin tức</a></li>
-            <li><a href="#">Các nhóm</a></li>
+            <li>
+				<a href="#">Các nhóm</a>
+				<ul class="sub_Group">
+					<li><a href="#">Nhóm lập trình</a></li>
+					<li><a href="#">Nhóm E-Spost</a></li>
+					<li><a href="#">Nhóm Office</a></li>
+					<li><a href="#">Nhóm Đồ Hoạ</a></li>
+					<li><a href="#">Nhóm UD Công nghệ</a></li>
+				</ul>
+			</li>
             <li><a href="#">Diễn đàn</a></li>
 		</ul>
 	</div>
@@ -13,7 +22,7 @@
 	<div class="right">
 		<ul>
 			<li><a href="#">Hoàng Hiệp</a></li>
-			<!--<li><a href="#">Đăng nhập</a></li>-->
+			<li><a href="#">Đăng nhập</a></li>
             <li><a href="#">Đăng xuất</a></li>
 		</ul>
 	</div>
