e64a97dadfae8e2941cc28685c0d4401ac73c428
HoangHiep2001HVU <84690670+HoangHiep2001HVU@users.noreply.github.com>
2021-09-26 23:46:41 +0700

    up lại csdl

--- data/databases/database/clb_tinhoc.sql
+++ data/databases/database/clb_tinhoc.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th9 26, 2021 lúc 06:42 PM
+-- Thời gian đã tạo: Th9 26, 2021 lúc 06:46 PM
 -- Phiên bản máy phục vụ: 10.4.20-MariaDB
 -- Phiên bản PHP: 7.3.29
 
@@ -20,6 +20,50 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `clb_tinhoc`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `account`
+--
+
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `role` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `account`
+--
+
+INSERT INTO `account` (`id`, `email`, `password`, `role`, `created_at`) VALUES
+(7, 'admin@123.com', 'e10adc3949ba59abbe56e057f20f883e', 1, '2021-09-22 14:35:31'),
+(8, 'ly@123.com', 'e10adc3949ba59abbe56e057f20f883e', 0, '2021-09-22 16:34:41');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `groups`
+--
+
+CREATE TABLE `groups` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `groups`
+--
+
+INSERT INTO `groups` (`id`, `name`) VALUES
+(1, 'Nhóm lập trình'),
+(2, 'Nhóm E-Spost'),
+(3, 'Nhóm Office'),
+(4, 'Nhóm Đồ Hoạ'),
+(5, 'Nhóm ƯD Công nghệ');
 
 -- --------------------------------------------------------
 
@@ -45,9 +89,46 @@ INSERT INTO `slide_imgs` (`id`, `name`, `image`, `note`, `created_at`) VALUES
 (6, 'Wellcom to the CLUB TIN HOC', 'img_1.jpg', 'Thử lần 3', '2021-09-24 15:30:55'),
 (26, 'Wellcom to the CLUB TIN HOC', 'clb.png', '', '2021-09-26 15:12:09');
 
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `birthday` datetime NOT NULL,
+  `email` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `class` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sex` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `birthday`, `email`, `class`, `sex`, `created_at`) VALUES
+(7, 'Giang Ly', '2021-09-02 00:00:00', 'admin@123.com', 'CNTT', 'Nữ', '2021-09-22 14:35:31'),
+(8, 'Giang Ly', '2021-09-10 00:00:00', 'ly@123.com', 'CNTT', 'Nam', '2021-09-22 16:34:41');
+
 --
 -- Chỉ mục cho các bảng đã đổ
 --
+
+--
+-- Chỉ mục cho bảng `account`
+--
+ALTER TABLE `account`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- Chỉ mục cho bảng `groups`
+--
+ALTER TABLE `groups`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Chỉ mục cho bảng `slide_imgs`
@@ -56,14 +137,49 @@ ALTER TABLE `slide_imgs`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
+
+--
+-- AUTO_INCREMENT cho bảng `account`
+--
+ALTER TABLE `account`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT cho bảng `groups`
+--
+ALTER TABLE `groups`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT cho bảng `slide_imgs`
 --
 ALTER TABLE `slide_imgs`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `email` FOREIGN KEY (`email`) REFERENCES `account` (`email`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
