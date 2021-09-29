1e5e7ace4dafac08f9f950100a68cc5b5f31f906
HoangHiep2001HVU <84690670+HoangHiep2001HVU@users.noreply.github.com>
2021-09-29 16:40:38 +0700

    làm phần body

--- layouts/body/body_home.php
+++ layouts/body/body_home.php
@@ -1,12 +1,12 @@
 <div class="main">
 	<div class="even">
 		<div class="img">
-			<img src="public/img/even_img/baner2.jpg" alt="Sự kiện 1">
+			<img src="public/img/even_img/bg_4.jpg" alt="Sự kiện 1" onclick="click_img(this.id,this.src,this.alt);">
 		</div>
 	</div>
 	<div class="even">
 		<div class="img">
-			<img src="public/img/even_img/baner2.jpg" alt="Sự kiện 1">
+			<img src="public/img/even_img/bg_5.jpg" alt="Sự kiện 1" onclick="click_img(this.id,this.src,this.alt);">
 		</div>
 	</div>
 </div>
--- layouts/body/click_img.php
+++ layouts/body/click_img.php
@@ -1,6 +1,8 @@
 <!--Khi click vào ảnh thì xuất hiện-->
-<div id="myModal" class="modal">
-	<span class="close" id= "click_img">&times;</span>
-	<img class="modal-content" id="img01">
-	<div id="caption"></div>
+<div class="click_img">
+	<div id="myModal" class="modal">
+		<span class="close" id= "click_img">&times;</span>
+		<img class="modal-content" id="img01">
+		<div id="caption"></div>
+	</div>
 </div>
