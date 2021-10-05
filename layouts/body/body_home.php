97b9d2fccf6ad9d18c7263f5f9ce5fb10c4058e7
HoangHiep2001HVU <84690670+HoangHiep2001HVU@users.noreply.github.com>
2021-10-06 01:28:41 +0700

    hoàn thành login logout

--- layouts/body/body_home.php
+++ layouts/body/body_home.php
@@ -3,56 +3,17 @@
 		<div class="col l-8 m-8 c-8">
 			<h1>=> Sự kiện đang diễn ra</h1>
 		</div>
-		<div class="col l-4 m-4 c-4">
-			<button class="insert_even">+ Thêm sự kiện</button>
-		</div>
+		<?php
+		if ($role == 1) {
+			echo "
+					<div class='col l-4 m-4 c-4'>
+						<button class='insert_even'>+ Thêm sự kiện</button>
+					</div>
+				";
+		}
+		?>
 	</div>
 	<div class="row">
-        <div class="col l-6 m-6 c-12">
-			<div class="even">
-				<img src="public/img/even_img/bg_4.jpg" alt="Sự kiện 1" onclick="click_img(this.id,this.src,this.alt);">
-				<div class="header_even">
-					<h2>Tiêu đề sự kiện</h2>
-					<p>Chi tiết sự kiện</p>
-					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
-					<div class="group_buttom">
-						<button>Chi tiết</button>
-						<button>Sửa</button>
-						<button>Xóa</button>
-					</div>
-				</div>
-			</div>
-        </div>
-        <div class="col l-6 m-6 c-12">
-			<div class="even">
-				<img src="public/img/even_img/bg_5.jpg" alt="Sự kiện 4" onclick="click_img(this.id,this.src,this.alt);">
-				<div class="header_even">
-					<h2>Tiêu đề sự kiện</h2>
-					<p>Chi tiết sự kiện</p>
-					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
-					<div class="group_buttom">
-						<button>Chi tiết</button>
-						<button>Sửa</button>
-						<button>Xóa</button>
-					</div>
-				</div>
-			</div>
-        </div>
-        <div class="col l-6 m-6 c-12">
-			<div class="even">
-				<img src="public/img/even_img/bg_5.jpg" alt="Sự kiện 4" onclick="click_img(this.id,this.src,this.alt);">
-				<div class="header_even">
-					<h2>Tiêu đề sự kiện</h2>
-					<p>Chi tiết sự kiện</p>
-					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
-					<div class="group_buttom">
-						<button>Chi tiết</button>
-						<button>Sửa</button>
-						<button>Xóa</button>
-					</div>
-				</div>
-			</div>
-        </div>
 		<div class="col l-6 m-6 c-12">
 			<div class="even">
 				<img src="public/img/even_img/bg_4.jpg" alt="Sự kiện 1" onclick="click_img(this.id,this.src,this.alt);">
@@ -62,12 +23,60 @@
 					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
 					<div class="group_buttom">
 						<button>Chi tiết</button>
-						<button>Sửa</button>
-						<button>Xóa</button>
+						<?php
+							if ($role == 1) {
+								echo "
+									<button>Sửa</button>
+									<button>Xóa</button>
+								";
+							}
+						?>
 					</div>
 				</div>
 			</div>
-        </div>
+		</div>
+		<div class="col l-6 m-6 c-12">
+			<div class="even">
+				<img src="public/img/even_img/bg_5.jpg" alt="Sự kiện 4" onclick="click_img(this.id,this.src,this.alt);">
+				<div class="header_even">
+					<h2>Tiêu đề sự kiện</h2>
+					<p>Chi tiết sự kiện</p>
+					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
+					<div class="group_buttom">
+						<button>Chi tiết</button>
+						<?php
+							if ($role == 1) {
+								echo "
+									<button>Sửa</button>
+									<button>Xóa</button>
+								";
+							}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col l-6 m-6 c-12">
+			<div class="even">
+				<img src="public/img/even_img/bg_5.jpg" alt="Sự kiện 4" onclick="click_img(this.id,this.src,this.alt);">
+				<div class="header_even">
+					<h2>Tiêu đề sự kiện</h2>
+					<p>Chi tiết sự kiện</p>
+					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
+					<div class="group_buttom">
+						<button>Chi tiết</button>
+						<?php
+							if ($role == 1) {
+								echo "
+									<button>Sửa</button>
+									<button>Xóa</button>
+								";
+							}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="col l-6 m-6 c-12">
 			<div class="even">
 				<img src="public/img/even_img/bg_4.jpg" alt="Sự kiện 1" onclick="click_img(this.id,this.src,this.alt);">
@@ -77,42 +86,18 @@
 					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
 					<div class="group_buttom">
 						<button>Chi tiết</button>
-						<button>Sửa</button>
-						<button>Xóa</button>
+						<?php
+							if ($role == 1) {
+								echo "
+									<button>Sửa</button>
+									<button>Xóa</button>
+								";
+							}
+						?>
 					</div>
 				</div>
 			</div>
-        </div>
-        <div class="col l-6 m-6 c-12">
-			<div class="even">
-				<img src="public/img/even_img/bg_5.jpg" alt="Sự kiện 4" onclick="click_img(this.id,this.src,this.alt);">
-				<div class="header_even">
-					<h2>Tiêu đề sự kiện</h2>
-					<p>Chi tiết sự kiện</p>
-					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
-					<div class="group_buttom">
-						<button>Chi tiết</button>
-						<button>Sửa</button>
-						<button>Xóa</button>
-					</div>
-				</div>
-			</div>
-        </div>
-        <div class="col l-6 m-6 c-12">
-			<div class="even">
-				<img src="public/img/even_img/bg_5.jpg" alt="Sự kiện 4" onclick="click_img(this.id,this.src,this.alt);">
-				<div class="header_even">
-					<h2>Tiêu đề sự kiện</h2>
-					<p>Chi tiết sự kiện</p>
-					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
-					<div class="group_buttom">
-						<button>Chi tiết</button>
-						<button>Sửa</button>
-						<button>Xóa</button>
-					</div>
-				</div>
-			</div>
-        </div>
+		</div>
 		<div class="col l-6 m-6 c-12">
 			<div class="even">
 				<img src="public/img/even_img/bg_4.jpg" alt="Sự kiện 1" onclick="click_img(this.id,this.src,this.alt);">
@@ -122,11 +107,80 @@
 					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
 					<div class="group_buttom">
 						<button>Chi tiết</button>
-						<button>Sửa</button>
-						<button>Xóa</button>
+						<?php
+							if ($role == 1) {
+								echo "
+									<button>Sửa</button>
+									<button>Xóa</button>
+								";
+							}
+						?>
 					</div>
 				</div>
 			</div>
-        </div>
-    </div>
+		</div>
+		<div class="col l-6 m-6 c-12">
+			<div class="even">
+				<img src="public/img/even_img/bg_5.jpg" alt="Sự kiện 4" onclick="click_img(this.id,this.src,this.alt);">
+				<div class="header_even">
+					<h2>Tiêu đề sự kiện</h2>
+					<p>Chi tiết sự kiện</p>
+					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
+					<div class="group_buttom">
+						<button>Chi tiết</button>
+						<?php
+							if ($role == 1) {
+								echo "
+									<button>Sửa</button>
+									<button>Xóa</button>
+								";
+							}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col l-6 m-6 c-12">
+			<div class="even">
+				<img src="public/img/even_img/bg_5.jpg" alt="Sự kiện 4" onclick="click_img(this.id,this.src,this.alt);">
+				<div class="header_even">
+					<h2>Tiêu đề sự kiện</h2>
+					<p>Chi tiết sự kiện</p>
+					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
+					<div class="group_buttom">
+						<button>Chi tiết</button>
+						<?php
+							if ($role == 1) {
+								echo "
+									<button>Sửa</button>
+									<button>Xóa</button>
+								";
+							}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="col l-6 m-6 c-12">
+			<div class="even">
+				<img src="public/img/even_img/bg_4.jpg" alt="Sự kiện 1" onclick="click_img(this.id,this.src,this.alt);">
+				<div class="header_even">
+					<h2>Tiêu đề sự kiện</h2>
+					<p>Chi tiết sự kiện</p>
+					<p>Thời gian: 2/10/2021 - 31/10/2021</p>
+					<div class="group_buttom">
+						<button>Chi tiết</button>
+						<?php
+							if ($role == 1) {
+								echo "
+									<button>Sửa</button>
+									<button>Xóa</button>
+								";
+							}
+						?>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
