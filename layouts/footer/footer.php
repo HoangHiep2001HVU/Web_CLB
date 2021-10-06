6e59132448ea757b9299d6e9c000a18e5f51b3c2
HoangHiep2001HVU <84690670+HoangHiep2001HVU@users.noreply.github.com>
2021-10-06 07:18:16 +0700

    sửa đường dẫn

--- layouts/footer/footer.php
+++ layouts/footer/footer.php
@@ -1,19 +1,19 @@
 <div class="footer grid wide">
 	<div class="row">
 		<div class="contact col l-4 m-6 c-6">
-			<img src="../../public/img/clb.png" alt="Ảnh CLB">
+			<img src="public/img/clb.png" alt="Ảnh CLB">
 			<p>Xây dựng bởi thành viên CLB Tin Học</p>
 		</div>
 		<div class="contact col l-2 m-6 c-6">
-			<img src="../../public/img/logo_fb.png" alt="Logo Facebook">
+			<img src="public/img/logo_fb.png" alt="Logo Facebook">
 			<p><a href="https://www.facebook.com/clbtin" target="black">CLB Tin Học</a></p>
 		</div>
 		<div class="contact col l-3 m-6 c-6">
-			<img src="../../public/img/logo_phone.png" alt="Logo Facebook">
+			<img src="public/img/logo_phone.png" alt="Logo phone">
 			<p>SĐT: 0384191013</p>
 		</div>
 		<div class="contact col l-3 m-6 c-6">
-			<img src="../../public/img/email.png" alt="Logo Facebook">
+			<img src="public/img/email.png" alt="Logo email">
 			<p>Email: clbtinhoc@mail.com</p>
 		</div>
 	</div>
